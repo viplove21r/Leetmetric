9ee37121d7ce0b5b054ee04f8c726e6e346737b3
Viplove Rajora <rajoraviplove21@gmail.com>
2026-07-16 10:26:58 +0530

    img

--- backend/server.js
+++ backend/server.js
@@ -86,6 +86,8 @@ query userProfile($username: String!) {
 });
 
 
-app.listen(3000,()=>{
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
 });
